3e0d6a2b4d7eca2fe9b697c3df436890cd99be18
Igor Cuckovic <cuckovic@gmail.com>
2020-02-11 18:35:14 +0100

    Updates

---
+++ precache-manifest.6abb648bdef52bbee9e6633f3bb437e6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa0b8b9a8a5d7d77072acfbc0549ffbe",
+    "url": "/mondrian-generator/index.html"
+  },
+  {
+    "revision": "25de6ad896f0c62854ac",
+    "url": "/mondrian-generator/static/css/main.c8b6ef5e.chunk.css"
+  },
+  {
+    "revision": "4e886aedd54d06eb82aa",
+    "url": "/mondrian-generator/static/js/2.83a2d7cc.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/mondrian-generator/static/js/2.83a2d7cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25de6ad896f0c62854ac",
+    "url": "/mondrian-generator/static/js/main.ca051284.chunk.js"
+  },
+  {
+    "revision": "73ca852a5b1b8a683d99",
+    "url": "/mondrian-generator/static/js/runtime-main.7d7dbf21.js"
+  }
+]);
